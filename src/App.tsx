2127f442d620b64f175cad75631cc5450309e6a9
Roman-Romanko <romanromanko796@gmail.com>
2025-01-17 22:19:29 +0200

    Page styling

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <NavBar />
+      {/* <NavBar /> */}
       <Routes>
         <Route path="/" element={<RegisterForm />} />
       </Routes>
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Nav } from '../../styles/NavBar-styled';
 
 export const NavBar = () => {
     return (
-        <div>
+        <Nav>
             <Link to="/">Home</Link>
             <Link to="/about">About</Link>
             <Link to="/contact">Contact</Link>
-        </div>
+        </Nav>
     );
 }
